3d41a5b0443c8c443dce83f6c6babd0145e217bf
AndreyKarlukov <karl911291@yandex.ru>
2020-11-23 09:39:34 +0300

    Using the configuration

--- EmailSender/EmailSender/EmaiSenderService.cs
+++ EmailSender/EmailSender/EmaiSenderService.cs
@@ -1,29 +1,33 @@
 ﻿using EmailExchanging;
+using EmailSender.Configuration;
 using MailKit.Net.Smtp;
 using MimeKit;
 
 namespace EmailSender
 {
     public class EmaiSenderService
-    {     
+    {
+        MailSettings  _mailSettings;
+        public EmaiSenderService()
+        {
+            _mailSettings = DiContainer.GetService<MailSettings>();
+        }
         public void SendEmail(EmailInputModel _emailInputModel)
         {
             var emailMessage = new MimeMessage();
 
-            emailMessage.From.Add(new MailboxAddress("DevEducation", "ruberd724@gmail.com"));
+            emailMessage.From.Add(new MailboxAddress(_mailSettings.SenderName, _mailSettings.SenderEmail));
             emailMessage.To.Add(new MailboxAddress("", _emailInputModel.Email));
-            emailMessage.Subject = "Тест";
+            emailMessage.Subject = _mailSettings.MessageHeader;
             emailMessage.Body = new TextPart(MimeKit.Text.TextFormat.Html)
             {
                 Text = _emailInputModel.Message
             };
             using (var client = new SmtpClient())
             {
-                client.Connect("smtp.gmail.com", 587, false);
-
-                client.Authenticate("ruberd724@gmail.com", "!@14kLm703");
+                client.Connect(_mailSettings.SmtpServer, _mailSettings.Port, false);
+                client.Authenticate(_mailSettings.SenderEmail, _mailSettings.Password);
                 client.Send(emailMessage);
-
                 client.Disconnect(true);
             }
         }      
